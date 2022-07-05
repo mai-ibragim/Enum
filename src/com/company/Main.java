8b71bf5da32ff08ccf992c9a93f1329f8584c615
Gulmairam <104002351+maybeIb@users.noreply.github.com>
2022-07-05 18:15:31 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (Language lang: Language.values()) {
+            System.out.println(lang);
+        }
+    }
+}
